1263b59aa80ae78a9fd3044392847e30f116e2d4
Patrick Magee <patrick.magee@live.co.uk>
2021-04-21 15:08:51 +0100

    code improvements

--- ThotBlock.ChromeExtension/begone.ts
+++ ThotBlock.ChromeExtension/begone.ts
@@ -1,23 +1,12 @@
-let thots: string[] = [];
-
 const url = 'https://raw.githubusercontent.com/pjmagee/thot-blocker-chrome-extension/master/ThotBlock.ChromeExtension/streamers.json';
 
-chrome.storage.local.get({ thots: [] }, (items: { thots: string[] }) => {
+type Data = {
+    streamers: string[];
+    expires: number | null;
+};
 
-    if (items.thots.length === 0) {
-        fetch(url).then(response => response.text()).then(json => {
-            thots = JSON.parse(json);
-            chrome.storage.local.set({ thots: thots });
-        });
-    }
-    else {
-        thots = items.thots;
-    }
-});
 
-const container: Element = document.getElementsByTagName("body").item(0);
-
-function begon(container: Element): void {
+function begone(data: Data, container: Element): void {
 
     const elements: HTMLCollectionOf<Element> = container.getElementsByClassName("tw-link");
 
@@ -26,7 +15,7 @@ function begon(container: Element): void {
         const anchor = elements.item(i) as HTMLAnchorElement;
         const channel = anchor.getAttribute('href')?.replace('/', '');
 
-        if (thots.includes(channel)) {
+        if (data.streamers.includes(channel)) {
 
             let current: HTMLElement = anchor;
             while (current.isConnected) {
@@ -40,31 +29,57 @@ function begon(container: Element): void {
     }
 }
 
-function register(): void {
+function register(data: Data): void {
 
-    const left = document.getElementsByClassName("tw-transition-group");
-    const towers = document.getElementsByClassName("tw-tower");
+    const suggested: HTMLCollectionOf<Element> = document.getElementsByClassName("tw-transition-group");
+    const cards: HTMLCollectionOf<Element> = document.getElementsByClassName("tw-tower");
+    const containers: Element[] = [];
 
-    const containers = [];
-
-    for (let i = 0; i < left.length; i++) {
-        containers.push(left.item(i));
+    for (let i = 0; i < suggested.length; i++) {
+        containers.push(suggested.item(i));
     }
 
-    for (let i = 0; i < towers.length; i++) {
-        containers.push(towers.item(i));
+    for (let i = 0; i < cards.length; i++) {
+        containers.push(cards.item(i));
     }
-
 
     for (let i = 0; i < containers.length; i++) {
         const container = containers[i];
-        begon(container);
-        const collectionObserver = new MutationObserver(() => begon(container));
+        begone(data, container);
+        const collectionObserver = new MutationObserver(() => begone(data, container));
         collectionObserver.observe(container, { attributes: false, childList: true, subtree: true });
     }
 }
 
-register();
+function initialize(): void {
 
-const bodyObserver = new MutationObserver(register);
-bodyObserver.observe(container, { attributes: false, childList: true, subtree: true });
+    let data: Data = {
+        streamers: [],
+        expires: null
+    }
+
+    chrome.storage.local.get(data, (result: Data) => {
+        const now = new Date();
+        if (result.streamers.length === 0 || result.expires === null || result.expires < now.getDate()) {
+            fetch(url).then(response => response.text()).then(json => {
+                const tomorrow = new Date();
+                tomorrow.setDate(now.getDate() + 1);
+                data.expires = tomorrow.getDate();
+                data.streamers = JSON.parse(json);
+                chrome.storage.local.set(data);
+            });
+        }
+        else {
+            data = result;
+        }
+    });
+
+    const container: Element = document.getElementsByTagName("body").item(0);
+
+    register(data);
+
+    const bodyObserver = new MutationObserver(() => register(data));
+    bodyObserver.observe(container, { attributes: false, childList: true, subtree: true });
+}
+
+initialize();
